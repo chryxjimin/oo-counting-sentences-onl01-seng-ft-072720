294951303317258463db8117a4b2d79aa5cd6a6e
Christie Yoon <cleex1999@gmail.com>
2020-08-09 00:13:28 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,11 +28,11 @@ class String
   end
 
   def count_sentences
-     if self.count_sentences == 0 
+     if self.count_sentences == 0
        puts "0"
      elsif self.count_sentences == 1
        puts "1"
-     else 
+     else
        puts "self.split("!", ".", "?") << self.count"
   end
   #binding.pry
